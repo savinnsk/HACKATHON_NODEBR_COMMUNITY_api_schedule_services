4d8a27d0bba18865d191288400635a11b8af1df1
Savio Picanço <saviopbrito@outlook.com>
2022-05-17 23:53:36 -0300

    middleware autenticação

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "@modules/users/infra/repositories/UsersRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+  // from header request endpoint
+  if (!authHeader) {
+    throw new AppError("Token missing");
+  }
+
+  // [0] = bearer
+  // [1] = jwt
+  const [, token] = authHeader.split(" ");
+
+  // verify token is valid
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      // secret key
+      "9eb71ab7420eb452a22787ca4fab501b"
+    ) as IPayload;
+
+    console.log(user_id);
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+    next();
+  } catch (err) {
+    throw new AppError("invalid token");
+  }
+}
