7bb66eea36b64174f4b0a0dfb3ca571f0099f8bc
Henrique Silva <kinho.sjdr@gmail.com>
2022-05-30 17:07:50 -0300

    adjusted variable host of method createConnection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import ORMCONFIG from '../../../../ormconfig.json'
 
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (host :string = undefined): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: "localhost",
+      host: host ?? ORMCONFIG.host,
       database: defaultOptions.database,
     })
   );
